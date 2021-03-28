84d4563f07dabd982cff2235662e8c08368a6ff4
9W3R7Y <48379338+9W3R7Y@users.noreply.github.com>
2021-03-29 05:40:36 +0900

    app_nameの設定

--- hoge/urls.py
+++ hoge/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "hoge"
 urlpatterns = [
     path('', views.index, name='index'),
-    path('RenderTemplate', views.RenderTemplateExample, name='renderTemplate'),
-    path('<int:num>', views.PathConverterExample, name='Path Converter'),
+    path('RenderTemplate', views.RenderTemplateExample, name='RenderTemplate'),
+    path('<int:num>', views.PathConverterExample, name='PathConverter'),
 ]
